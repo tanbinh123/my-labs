0a31f0d038b989d2a090249b769d0ea6ec2cdaa4
samurai-of-honor <67967010+samurai-of-honor@users.noreply.github.com>
2020-11-05 08:39:17 +0200

    Update 1variation.c

--- Lab_1.2/1variation.c
+++ Lab_1.2/1variation.c
@@ -2,17 +2,20 @@
 #include <math.h>
 
 int main () {
-    int n;
+    int n, counter = 0;
     double multiply = 1, result = 0;
     printf ("Enter N: ");
     scanf ("%d", &n);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             multiply *= j + cos (j);
+            counter ++;
         }
         result += (pow (4, i) - i) / multiply;
         multiply = 1;
+        counter ++;
     }
-    printf ("Result: %.7lf", result);
+    printf ("Result: %.7lf \n", result);
+    printf ("Number of operations: %d", counter);
     return 0;
 }

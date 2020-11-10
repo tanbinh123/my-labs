9316bb10f1edaded0ac97e85a83cc0bf6c3f00b1
samurai-of-honor <67967010+samurai-of-honor@users.noreply.github.com>
2020-11-10 18:01:55 +0200

    Replacing "pow", fixed counter

--- Lab_1.2/2variation.c
+++ Lab_1.2/2variation.c
@@ -3,13 +3,14 @@
 
 int main () {
     int n, counter = 0;
-    double multiply = 1, result = 0;
+    double multiply = 1, degree = 1, result = 0;
     printf ("Enter N: ");
     scanf ("%d", &n);
     for (int i = 1; i <= n; i++) {
             multiply *= i + cos (i);
-            result += (pow (4, i) - i) / multiply;
-            counter ++;
+            degree *= 4;
+            result += (degree - i) / multiply;
+            counter += 7;
         }
     printf ("Result: %.7lf\n", result);
     printf ("Number of operations: %d", counter);

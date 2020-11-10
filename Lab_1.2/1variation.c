158545674c413c5ab2b142015f76823ffb5790dd
samurai-of-honor <67967010+samurai-of-honor@users.noreply.github.com>
2020-11-10 17:52:56 +0200

    Update 1variation.c
    Replacing "pow" by multiplication. Fixed operation counter.

--- Lab_1.2/1variation.c
+++ Lab_1.2/1variation.c
@@ -3,17 +3,18 @@
 
 int main () {
     int n, counter = 0;
-    double multiply = 1, result = 0;
+    double multiply = 1, degree = 1, result = 0;
     printf ("Enter N: ");
     scanf ("%d", &n);
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= i; j++) {
             multiply *= j + cos (j);
-            counter ++;
+            counter += 3;
         }
-        result += (pow (4, i) - i) / multiply;
+        degree *= 4;
+        result += (degree - i) / multiply;
         multiply = 1;
-        counter ++;
+        counter += 4;
     }
     printf ("Result: %.7lf \n", result);
     printf ("Number of operations: %d", counter);

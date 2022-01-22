f56142a9c18d6042604223009279237613cf2e56
Pasha Baran <pashabaran1207@gmail.com>
2022-01-22 10:21:03 +0200

    reloading two labs with ignore vs files#2.1

--- OOP/Lab1/Lab1/module1.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "framework.h"
-
-int Func_Work1(HINSTANCE hInst, HWND hWnd, WCHAR* dest);
--- OOP/Lab1/Lab1/module2_1.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "framework.h"
-
-int Func_Work2_1(HINSTANCE hInst, HWND hWnd);
